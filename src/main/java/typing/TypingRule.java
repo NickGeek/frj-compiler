92e6f7a01606472af22edbb945c31b9297a1f66b
Nick Webster <nick@nick.geek.nz>
2020-07-31 13:52:35 +1200

    More prototyping for the type checker, getting closer to what likely will be my approach.

--- src/main/java/typing/TypingRule.java
+++ src/main/java/typing/TypingRule.java
@@ -7,20 +7,38 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public interface TypingRule<T extends Expression> {
-	static Map<Expression.IdentifierExpr, TypedExpression<?>> type(Program program) {
-		var rules = new TypingRules(program);
-		program.visit(visitable -> {
-			if (visitable instanceof Expression) {
-				rules.type((Expression) visitable);
-			}
-		});
-
-		return null; // TODO: All of this
-	}
+//	static Map<Expression, TypedExpression<?>> type(Program program) {
+//		var rules = new TypingRules(program, new HashMap<>());
+//		program.visit(visitable -> {
+//			if (visitable instanceof Expression) {
+//				rules.type((Expression) visitable);
+//			}
+//		});
+//
+//		return null; // TODO: All of this
+//	}
 
 	static Optional<String> getErrors(Program program) {
 		var errors = new ArrayList<TypeError>();
 		var rules = new TypingRules(program);
+
+		// Is main okay?
+		var foo = rules.type(program.main);
+
+		// (class) rule
+		// TODO
+
+		// (interface) rule
+		// TODO
+
+		// (method) rule
+		// TODO
+
+//		program.visit(visitable -> {
+//			if (visitable instanceof Expression) {
+//				rules.type((Expression) visitable);
+//			}
+//		});
 
 		return !errors.isEmpty() ? Optional.of(errors.stream().map(TypeError::toString).collect(Collectors.joining("\n"))) : Optional.empty();
 	}
--- src/main/java/typing/TypingRules.java
+++ src/main/java/typing/TypingRules.java
@@ -7,57 +7,44 @@ import parser.ProgramNode;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
+import java.util.Objects;
 
 public class TypingRules {
 	private final Program program;
-	private final Map<Expression.IdentifierExpr, TypedExpression<?>> gamma;
+	private final Map<Expression.IdentifierExpr, ProgramNode.Type> gamma;
 
 	public TypingRules(Program program) {
 		this.program = program;
 		this.gamma = new HashMap<>();
 	}
 
-	public TypedExpression<?> type(Expression expression) {
-		return null;
+	public ProgramNode.Type type(Expression expr) {
+		if (expr instanceof Expression.IdentifierExpr) {
+			return this.typeVariable((Expression.IdentifierExpr) expr);
+		} else if (expr instanceof Expression.LiteralExpr<?>) {
+			return this.typeLiteral((Expression.LiteralExpr<?>) expr);
+		}
+
+		throw new IllegalArgumentException("No type rules cover: " + expr);
 	}
 
 	/**
 	 * Rule (x) types variables (identifiers) as the type they point to in the Gamma typing environment.
 	 */
-	private TypedExpression<Expression.IdentifierExpr> typeVariable(Expression.IdentifierExpr expr) {
-		var lookup = gamma.get(expr);
-
-		return new TypedExpression<>(
-				expr,
-				lookup.type
-		);
+	private ProgramNode.Type typeVariable(Expression.IdentifierExpr expr) {
+		var type = this.gamma.get(expr);
+		return new ProgramNode.Type(expr.pos(), type.mdf, type.isLifted, type.name);
 	}
 
 	/**
 	 * This rule isn't from the formalism, but literals have been added for convenience.
 	 */
-	private TypedExpression<Expression.LiteralExpr<?>> typeLiteral(Expression.LiteralExpr<?> expr) {
+	private ProgramNode.Type typeLiteral(Expression.LiteralExpr<?> expr) {
 		return switch (expr.type) {
-			case INTEGER -> new TypedExpression<>(
-					expr,
-					new ProgramNode.Type(expr.pos(), Modifier.IMM, false, "Int")
-			);
-
-			case FLOAT -> new TypedExpression<>(
-					expr,
-					new ProgramNode.Type(expr.pos(), Modifier.IMM, false, "Float")
-			);
-
-			case STRING -> new TypedExpression<>(
-					expr,
-					new ProgramNode.Type(expr.pos(), Modifier.IMM, false, "Str")
-			);
-
-			case BOOLEAN -> new TypedExpression<>(
-					expr,
-					new ProgramNode.Type(expr.pos(), Modifier.IMM, false, "Bool")
-			);
+			case INTEGER -> new ProgramNode.Type(expr.pos(), Modifier.IMM, false, "Int");
+			case FLOAT -> new ProgramNode.Type(expr.pos(), Modifier.IMM, false, "Float");
+			case STRING -> new ProgramNode.Type(expr.pos(), Modifier.IMM, false, "String");
+			case BOOLEAN -> new ProgramNode.Type(expr.pos(), Modifier.IMM, false, "Bool");
 		};
 	}
 }
